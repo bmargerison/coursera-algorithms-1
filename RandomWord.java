aac99a92cf6dcab88280add1018b1da68b900530
bmargerison <bmargerison@outlook.com>
2022-02-24 14:12:52 +0000

    Setup and intro

---
+++ RandomWord.java
@@ -0,0 +1,21 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        double inputs = 0.0;
+        String champion = "";
+        while (!StdIn.isEmpty()) {
+            inputs += 1.0;
+            String var = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / inputs)) { champion = var; }
+        }
+        System.out.println(champion);
+    }
+}
